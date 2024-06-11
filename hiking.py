eeb7e1a0878df60efb17a657419166c5411401fc
ColeCollector <124203328+ColeCollector@users.noreply.github.com>
2024-06-11 13:41:07 -0700

    bruh

--- hiking.py
+++ hiking.py
@@ -1,9 +1,9 @@
-from game import SCREEN_HEIGHT
 import pygame,random
 height = 800
 width = 500
 
-obstacle_width = 100
+# Obstacles properties
+obstacle_width = 115
 obstacle_height = 20
 obstacles = []
 
@@ -14,7 +14,12 @@ for i in range(7):
     obstacle_y = i*100
     obstacles.append(pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_height))
 
+obstacle_height = 500
+obstacle_width = 100
 
+obstacle_x = random.randint(200,300)
+obstacle_y = random.randint(0, 500)
+obstacles.append(pygame.Rect(obstacle_x, obstacle_y, obstacle_width, obstacle_height))
 
 # pygame setup
 pygame.init()
@@ -23,6 +28,8 @@ clock = pygame.time.Clock()
 running = True
 walkradius = 100
 down_pos = [width/2,700]
+hoptime = 0
+image = pygame.image.load('foot.png')
 
 while running:
     # poll for events
@@ -33,22 +40,45 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+
+
             if (down_pos[0]-pos[0])**2 + (down_pos[1]-pos[1])**2 < walkradius**2:
                 down_pos = pos
-            else:pass
+                for obstacle in obstacles:
+                    hoptime = 30
+            else:
 
+                pass
     
-    print(pos)
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-    pygame.draw.circle(screen,"white",down_pos,walkradius+1)
-    pygame.draw.circle(screen,"purple",down_pos,walkradius)
-    pygame.draw.circle(screen,"red",pos,20)
+    #Moving the obstacles when we move:
+    if hoptime > 0:
+        hoptime -= hoptime/3
+        if hoptime < 0.1:hoptime = 0
 
-    pygame.draw.circle(screen,"black",down_pos,20)
+        for obstacle in obstacles:
+            obstacle.y+=hoptime
+    
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("#87ceeb")
+    pygame.draw.circle(screen,"white",down_pos,walkradius+1)
+    pygame.draw.circle(screen,"#87ceeb",down_pos,walkradius)
+    pygame.draw.circle(screen,"red",pos,20)
     
 
     # RENDER YOUR GAME HERE
+    for obstacle in obstacles:
+        pygame.draw.rect(screen, "#879ceb", obstacle)
+
+        if obstacle.y > 800:
+            obstacle.y = 0
+            if obstacle.height < 500:
+                if random.randint(0,1) == 0: obstacle.x = random.randint(0,100)
+                else: obstacle.x = random.randint(400,500)
+            #else:
+            #    pass
+    
+    #pygame.draw.circle(screen,"black",down_pos,20)
+    screen.blit(image, (down_pos[0]-75,down_pos[1]-75))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
